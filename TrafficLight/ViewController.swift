4b0568b61fc4197efe3be12de984f3bed503f2c5
Viktor Teslenko <meex_v92@mail.ru>
2023-02-13 21:44:33 +0800

    We made the traffic light round

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var toggleButton: UIButton!
+    @IBOutlet var allColors: UIStackView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //Скруглим кнопку
+        toggleButton.layer.cornerRadius = 10
+        //сделаем светофор круглым
+        redLight.layer.cornerRadius = redLight.frame.size.width/2
+        redLight.clipsToBounds = true
+        
+        yellowLight.layer.cornerRadius = yellowLight.frame.size.width/2
+        yellowLight.clipsToBounds = true
+        
+        greenLight.layer.cornerRadius = greenLight.frame.size.width/2
+        greenLight.clipsToBounds = true
+        
+
     }
-
-
+    
+    
+    
+    @IBAction func makeButtonAction() {
+    }
 }
 
