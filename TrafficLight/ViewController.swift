d58138f94d1ce313299c68e7f053f2f3f679efc9
Viktor Teslenko <meex_v92@mail.ru>
2023-02-13 17:47:07 +0800

    add traffic lights and button

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var startButton: UIButton!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func makeButtonAction() {
+    }
+    
 
 }
 
