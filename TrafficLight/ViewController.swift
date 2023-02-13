c4f6319f799c43aa326c38fdf116f658fb1493de
Viktor Teslenko <meex_v92@mail.ru>
2023-02-13 20:20:22 +0800

    Aligned the elements in the center

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,21 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var redLight: UIView!
-    @IBOutlet var yellowLight: UIView!
-    @IBOutlet var greenLight: UIView!
-    @IBOutlet var startButton: UIButton!
-    
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    @IBAction func makeButtonAction() {
-    }
-    
+
 
 }
 
