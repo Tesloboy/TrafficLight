4262a6481eb031e87687e8cd9f2016327f13a641
Viktor Teslenko <meex_v92@mail.ru>
2023-02-20 09:36:45 +0800

    Added the ability to switch lights

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,15 +7,22 @@
 
 import UIKit
 
+enum CurrentLight {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
 
     
     @IBOutlet var redLight: UIView!
     @IBOutlet var yellowLight: UIView!
     @IBOutlet var greenLight: UIView!
-    @IBOutlet var toggleButton: UIButton!
-    @IBOutlet var allColors: UIStackView!
     
+    @IBOutlet var toggleButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private var lightIsOn: CGFloat = 1
+    private var lightIsOff: CGFloat = 0.3
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,22 +30,42 @@ class ViewController: UIViewController {
         
         //Скруглим кнопку
         toggleButton.layer.cornerRadius = 10
+        
+        redLight.alpha = lightIsOff
+        yellowLight.alpha = lightIsOff
+        greenLight.alpha = lightIsOff
+        
+        
         //сделаем светофор круглым
-        redLight.layer.cornerRadius = redLight.frame.size.width/2
-        redLight.clipsToBounds = true
+//        redLight.layer.cornerRadius = redLight.frame.size.width/2
+//        redLight.clipsToBounds = true
+//        
+//        yellowLight.layer.cornerRadius = yellowLight.frame.size.width/2
+//        yellowLight.clipsToBounds = true
+//        
+//        greenLight.layer.cornerRadius = greenLight.frame.size.width/2
+//        greenLight.clipsToBounds = true
         
-        yellowLight.layer.cornerRadius = yellowLight.frame.size.width/2
-        yellowLight.clipsToBounds = true
-        
-        greenLight.layer.cornerRadius = greenLight.frame.size.width/2
-        greenLight.clipsToBounds = true
-        
-
     }
     
-    
-    
     @IBAction func makeButtonAction() {
+       
+        toggleButton.setTitle("NEXT", for: .normal)
+
+        switch currentLight {
+        case .red:
+            greenLight.alpha = lightIsOff
+            redLight.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redLight.alpha = lightIsOff
+            yellowLight.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            yellowLight.alpha = lightIsOff
+            greenLight.alpha = lightIsOn
+            currentLight = .red
+        }
     }
 }
 
